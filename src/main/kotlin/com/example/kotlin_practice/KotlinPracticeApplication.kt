24ab2e5e1c3e89ffd3ce5de51fec659b5980db63
dkflfkd53 <b57062562@gmail.com>
2024-02-03 18:51:48 +0900

    🌱 :: first commit

---
+++ src/main/kotlin/com/example/kotlin_practice/KotlinPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_practice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
