0dd7de12821f28c7448bb9f35290d1d0413b2e37
dkflfkd53 <b57062562@gmail.com>
2024-02-08 18:03:30 +0900

    🌧 :: PropertiesScan 어노테이션 추가

--- src/main/kotlin/com/example/kotlin_practice/KotlinPracticeApplication.kt
+++ src/main/kotlin/com/example/kotlin_practice/KotlinPracticeApplication.kt
@@ -1,9 +1,11 @@
 package com.example.kotlin_practice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class KotlinPracticeApplication
 
 fun main(args: Array<String>) {
