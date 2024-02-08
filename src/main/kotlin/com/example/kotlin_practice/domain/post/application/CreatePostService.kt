3ee337948dcd94697e5ea231a4af902ec6b9d227
dkflfkd53 <b57062562@gmail.com>
2024-02-08 15:45:34 +0900

    🌧 :: 게시글 생성 api service 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/post/application/CreatePostService.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin_practice.domain.post.application
+
+import com.example.kotlin_practice.domain.post.dao.PostRepository
+import com.example.kotlin_practice.domain.post.domain.Post
+import com.example.kotlin_practice.domain.post.dto.request.PostRequest
+import org.springframework.stereotype.Service
+
+@Service
+class CreatePostService(
+    private val postRepository: PostRepository
+) {
+
+    fun createPost(request: PostRequest) {
+        postRepository.save(
+            Post(
+                title = request.title,
+                content = request.content
+            )
+        )
+    }
+}
