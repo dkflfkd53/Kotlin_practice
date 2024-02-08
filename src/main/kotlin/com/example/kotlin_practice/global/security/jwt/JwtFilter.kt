a1d32561037a1fa2dced0dd4e0262d527a08c699
dkflfkd53 <b57062562@gmail.com>
2024-02-08 18:00:31 +0900

    🌧 :: JwtFilter 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/global/security/jwt/JwtFilter.kt
@@ -0,0 +1,34 @@
+package com.example.kotlin_practice.global.security.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import lombok.RequiredArgsConstructor
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+
+
+@RequiredArgsConstructor
+class JwtFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+) : OncePerRequestFilter() {
+
+    @Throws(IOException::class, ServletException::class)
+    public override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token: String? = jwtTokenProvider.resolveToken(request)
+        if (token != null) {
+            // Jwt에 담긴 정보를 통해 어떤 유저가 로그인 되었는지 식별하고,
+            // Spring Security SecurityContextHolder를 통해 현재 요청에 유저의 정보를 등록한다.
+            val authentication: Authentication = jwtTokenProvider.getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
