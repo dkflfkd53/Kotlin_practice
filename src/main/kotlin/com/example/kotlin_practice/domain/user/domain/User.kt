4b1903b579a3c5b957db611e1232e40f8dfc08d5
dkflfkd53 <b57062562@gmail.com>
2024-02-08 17:59:26 +0900

    🌧 :: User entity 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/user/domain/User.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_practice.domain.user.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    val userName: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+}
