eb5fd3cf2effbcfebdd14c7b6f068513dd747c7c
dkflfkd53 <b57062562@gmail.com>
2024-02-08 15:45:57 +0900

    🌧 :: 게시글 repository 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/post/dao/PostRespository.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_practice.domain.post.dao
+
+import com.example.kotlin_practice.domain.post.domain.Post
+import org.springframework.data.repository.CrudRepository
+
+interface PostRepository : CrudRepository<Post, Long> {
+}
