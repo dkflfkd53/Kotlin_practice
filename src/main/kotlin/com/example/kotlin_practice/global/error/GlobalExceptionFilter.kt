cff2e0d86fa45dd3c25fd99a69a57404281449af
dkflfkd53 <b57062562@gmail.com>
2024-02-08 18:02:35 +0900

    🌧 :: GlobalExceptionFilter 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/global/error/GlobalExceptionFilter.kt
@@ -0,0 +1,44 @@
+package com.example.kotlin_practice.global.error
+
+import com.example.kotlin_practice.global.error.exception.BusinessException
+import com.example.kotlin_practice.global.error.exception.ErrorCode
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+
+class GlobalExceptionFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+
+    @Throws(IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            filterChain.doFilter(request, response)
+        } catch (e: BusinessException) {
+            val errorCode: ErrorCode = e.errorCode
+            writerErrorResponse(
+                response,
+                errorCode.statusCode,
+                ErrorResponse.of(errorCode, errorCode.message)
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            writerErrorResponse(response, response.status, ErrorResponse.of(response.status, e.message!!))
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun writerErrorResponse(response: HttpServletResponse, statusCode: Int, errorResponse: ErrorResponse) {
+        response.status = statusCode
+        response.contentType = "application/json"
+        response.characterEncoding = "UTF-8"
+        objectMapper!!.writeValue(response.writer, errorResponse)
+    }
+}
