2254361570b70f89c45f5eee3a19c762bedf1727
dkflfkd53 <b57062562@gmail.com>
2024-02-17 19:11:10 +0900

    ♻️ :: 게시글 entity 수정

--- src/main/kotlin/com/example/kotlin_practice/domain/post/domain/Port.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/post/domain/Port.kt
@@ -1,23 +1,14 @@
 package com.example.kotlin_practice.domain.post.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
+@Table(name = "post")
 class Post (
-    title: String,
-    content: String,
+    var title: String,
+    var content: String,
 ) {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0
-
-    var title = title
-        protected set
-
-    var content = content
-        protected set
+    val postId: Long = 0
 }
