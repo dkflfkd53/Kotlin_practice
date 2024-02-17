69f707a231cc3b241e760be8cf21d6803352cdee
dkflfkd53 <b57062562@gmail.com>
2024-02-17 21:26:07 +0900

    🌧 :: 로그인 api service 추가

--- src/main/kotlin/com/example/kotlin_practice/domain/user/api/UserController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/user/api/UserController.kt
@@ -1,19 +1,28 @@
 package com.example.kotlin_practice.domain.user.api
 
+import com.example.kotlin_practice.domain.user.application.LoginService
 import com.example.kotlin_practice.domain.user.application.SignupService
+import com.example.kotlin_practice.domain.user.dto.request.LoginRequest
 import com.example.kotlin_practice.domain.user.dto.request.SignupRequest
+import com.example.kotlin_practice.global.security.jwt.dto.response.TokenResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
 class UserController(
-    private val signupService: SignupService
+    private val signupService: SignupService,
+    private val loginService: LoginService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signup")
     fun signup(@RequestBody request: SignupRequest) {
         signupService.signup(request)
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody request: LoginRequest): TokenResponse {
+        return loginService.login(request)
     }
 }
