6ff7436c3ef51c1daac3eb8be919c78676cae7a8
dkflfkd53 <b57062562@gmail.com>
2024-02-17 19:09:13 +0900

    🌧 :: 게시글 조회 api controller 추가

--- src/main/kotlin/com/example/kotlin_practice/domain/post/api/PostController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/post/api/PostController.kt
@@ -1,23 +1,27 @@
 package com.example.kotlin_practice.domain.post.api
 
 import com.example.kotlin_practice.domain.post.application.CreatePostService
+import com.example.kotlin_practice.domain.post.application.ReadPostDetailsService
 import com.example.kotlin_practice.domain.post.dto.request.PostRequest
+import com.example.kotlin_practice.domain.post.dto.response.PostResponse
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/post")
 class PostController(
-    private val createPostService: CreatePostService
+    private val createPostService: CreatePostService,
+    private val readPostDetailsService: ReadPostDetailsService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createPost(@RequestBody request: PostRequest) {
         createPostService.createPost(request)
+    }
+
+    @GetMapping("/{postId}")
+    fun readPostDetails(@PathVariable postId: Long): PostResponse {
+        return readPostDetailsService.readPostDetails(postId)
     }
 }
