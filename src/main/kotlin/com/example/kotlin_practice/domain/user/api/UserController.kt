82c4fdd1c245618eda14b627c8ede398b2e658bc
dkflfkd53 <b57062562@gmail.com>
2024-02-17 21:04:50 +0900

    ♻️ :: 회원가입 controller 수정

--- src/main/kotlin/com/example/kotlin_practice/domain/user/api/UserController.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/user/api/UserController.kt
@@ -2,15 +2,17 @@ package com.example.kotlin_practice.domain.user.api
 
 import com.example.kotlin_practice.domain.user.application.SignupService
 import com.example.kotlin_practice.domain.user.dto.request.SignupRequest
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
 class UserController(
     private val signupService: SignupService
 ) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
     fun signup(@RequestBody request: SignupRequest) {
         signupService.signup(request)
     }
