2f0b5d6f930be4e7ce78f4f91187bec0d0ca145f
dkflfkd53 <b57062562@gmail.com>
2024-02-17 19:09:55 +0900

    🌧 :: 게시글 상세조회 api service 추가

---
+++ src/main/kotlin/com/example/kotlin_practice/domain/post/application/ReadPostDetailsService.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_practice.domain.post.application
+
+import com.example.kotlin_practice.domain.post.dao.PostRepository
+import com.example.kotlin_practice.domain.post.domain.Post
+import com.example.kotlin_practice.domain.post.dto.response.PostResponse
+import org.springframework.stereotype.Service
+
+@Service
+class ReadPostDetailsService(
+    private val postRepository: PostRepository
+) {
+
+    fun readPostDetails(postId: Long): PostResponse {
+        val post: Post = postRepository.findByPostId(postId)
+
+        return PostResponse(
+            postId = postId,
+            title = post.title,
+            content = post.content
+        )
+    }
+}
