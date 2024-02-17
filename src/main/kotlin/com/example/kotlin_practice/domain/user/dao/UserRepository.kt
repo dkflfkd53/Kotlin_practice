8a07f1891ad8c03d1b9452798d7702527823d5ef
dkflfkd53 <b57062562@gmail.com>
2024-02-17 21:26:35 +0900

    🌧 :: findByAccountId 메서드 추가

--- src/main/kotlin/com/example/kotlin_practice/domain/user/dao/UserRepository.kt
+++ src/main/kotlin/com/example/kotlin_practice/domain/user/dao/UserRepository.kt
@@ -4,5 +4,5 @@ import com.example.kotlin_practice.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository : CrudRepository<User, Long> {
-    fun findByUserName(userName: String): User
+    fun findByAccountId(accountId: String): User
 }
