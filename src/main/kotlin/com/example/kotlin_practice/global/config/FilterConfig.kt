8fd141835474052452d5ad17e7a83d2d3f04487f
dkflfkd53 <b57062562@gmail.com>
2024-03-29 11:46:36 +0900

    ♻️ :: FilterConfig 수정

--- src/main/kotlin/com/example/kotlin_practice/global/config/FilterConfig.kt
+++ src/main/kotlin/com/example/kotlin_practice/global/config/FilterConfig.kt
@@ -8,16 +8,18 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
 
+@Component
 class FilterConfig(
     private val jwtTokenProvider: JwtTokenProvider,
     private val objectMapper: ObjectMapper
-) : SecurityConfigurerAdapter<DefaultSecurityFilterChain?, HttpSecurity>() {
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
-    override fun configure(http: HttpSecurity) {
-        val jwtTokenFilter = JwtFilter(jwtTokenProvider)
-        val globalExceptionFilter = GlobalExceptionFilter(objectMapper)
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
-            .addFilterBefore(globalExceptionFilter, JwtFilter::class.java)
+    override fun configure(builder: HttpSecurity) {
+        builder.run {
+            addFilterBefore(JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter::class.java)
+            addFilterBefore(GlobalExceptionFilter(objectMapper), JwtFilter::class.java)
+        }
     }
 }
